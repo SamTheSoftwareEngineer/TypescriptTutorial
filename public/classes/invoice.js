69df62aacaa6d47d29e6c8f52f36160be25711f8
Samaira Stewart <stewartsamaira@gmail.com>
2024-05-06 20:17:11 -0400

    Contains and exports the invoice class

---
+++ public/classes/invoice.js
@@ -0,0 +1,16 @@
+// classes
+export class Invoice {
+    // readonly client: string;
+    // private details: string;
+    // public amount: number;
+    constructor(
+    // Access Modifiers
+    client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
+    }
+    format() {
+        return `${this.client} owes £${this.amount} for ${this.details}`;
+    }
+}
