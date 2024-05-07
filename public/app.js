550283fc60879b124b433d917a2cebbaafd897ba
Samaira Stewart <stewartsamaira@gmail.com>
2024-05-06 20:16:56 -0400

    Contains DOM and Type casting notes

--- public/app.js
+++ public/app.js
@@ -1,31 +1,16 @@
 "use strict";
-// The DOM and Type Casting 
-// ! = null or undefined
-// const form = document.querySelector('form')!;
-const form = document.querySelector('.new-item-form'); // Type Casting 
-console.log(form.children);
-// inputs
-const type = document.querySelector('#type');
-const tofrom = document.querySelector('#tofrom');
-const details = document.querySelector('#details');
-const amount = document.querySelector('#amount');
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
-});
-// Classes
-class invoice {
-    constructor(client, details, amount) {
-        this.client = client;
-        this.details = details;
-        this.amount = amount;
-        this.client = client;
-        this.details = details;
-        this.amount = amount;
+// // The DOM and Type Casting 
+const me = {
+    name: 'steve',
+    age: 30,
+    speak(text) {
+        console.log(text);
+    },
+    spend(amount) {
+        console.log('I spent', amount);
+        return amount;
     }
-    format() {
-        return `${this.client} owes £${this.amount} for ${this.details}`;
-    }
-}
-const InvoiceOne = new invoice('mario', 'work on the mario website', 250);
-console.log(InvoiceOne);
+};
+const greet = (person) => {
+    console.log('hello', person.name);
+};
